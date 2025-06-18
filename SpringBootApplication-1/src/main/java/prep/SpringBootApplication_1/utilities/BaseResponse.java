9a49bd9850a020b7736a4b400e24b244491a5e0a
rakesh-dev <rakesh.r@apcfss.in>
2025-06-18 12:03:43 +0530

    added some common utils that are maintained in projects

---
+++ SpringBootApplication-1/src/main/java/prep/SpringBootApplication_1/utilities/BaseResponse.java
@@ -0,0 +1,53 @@
+package prep.SpringBootApplication_1.utilities;
+
+
+import lombok.Data;
+
+@Data
+public class BaseResponse<T> {
+
+    private int status;
+
+    private String desc;
+
+    private T body;
+
+    public BaseResponse(int status, String success, T dataSaved) {
+        this.status = status;
+        this.desc = success;
+        this.body = dataSaved;
+    }
+
+    public BaseResponse() {
+
+    }
+
+    public BaseResponse<T> createResponse(int status, String desc, T body){
+        this.status = status;
+        this.desc = desc;
+        this.body = body;
+        return this;
+    }
+    public BaseResponse<T> createResponse(int status, T body){
+        this.status = status;
+        switch (status){
+            case 200:
+                this.desc = "OK";
+                break;
+            case 400:
+                this.desc = "Bad Request";
+                break;
+            case 404:
+                this.desc = "Not Found";
+                break;
+            case 401:
+                this.desc = "Unauthorized Access";
+                break;
+            default:
+                this.desc = "Unknown Status";
+                break;
+        }
+        this.body = body;
+        return this;
+    }
+}
